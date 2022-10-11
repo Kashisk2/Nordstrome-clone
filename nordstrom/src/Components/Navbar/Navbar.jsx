e6681fce6673dcfb46dd62791cc10ffc503b5236
Kashisk2 <kashiramshekhda2010@gmail.com>
2022-10-12 02:53:43 +0530

    start

--- nordstrom/src/Components/Navbar/Navbar.jsx
+++ nordstrom/src/Components/Navbar/Navbar.jsx
@@ -24,6 +24,7 @@ import HolydaySales from "../../Images/HolydaySales.png";
 import { useRef } from "react";
 import InputBox from "./InputBox";
 import SignInButton from "./SignInButton";
+import NavbarTab from "./NavbarTab";
 
 export default function Navbar() {
   return (
@@ -32,7 +33,9 @@ export default function Navbar() {
         width={"95%"}
         m={"auto"}
         mt={"25px"}
+        mb={"60px"}
         backgroundColor={"transparent"}
+
       >
         <Flex justifyContent={"space-between"} width={"100%"} mb={"15px"}>
           <Box>
@@ -42,64 +45,14 @@ export default function Navbar() {
               src="https://i.imgur.com/ubtddUO.png"
             />
           </Box>
-       <InputBox/>
-         <SignInButton/>
+          <InputBox />
+          <SignInButton />
         </Flex>
+        <VStack  width={"100%"} gap={'20px'}>
+
         <Box width={"100%"} h={"0.4px"} backgroundColor={"gray.300"}></Box>
-        <Flex w={"80%"} justifyContent={"space-between"}>
-          <Menu>
-            <MenuButton
-              _hover={{ borderBottom: "2px solid black" }}
-              height={"26px"}
-              border={"2px solid transparent"}
-              color={"rgb(51,51,51)"}
-              lineHeight={"22.5px"}
-            >
-              Open menu
-            </MenuButton>
-            <Portal>
-              <Box width={"100%"}>
-                <MenuList
-                  boxShadow="lg"
-                  border={"none"}
-                  width="1200px"
-                  p={"20px"}
-                >
-                  <Flex
-                    width={"100%"}
-                    m={"auto"}
-                    justifyContent={"space-between"}
-                  >
-                    <Box fontWeight={"800"}>
-                      <Image width={"100%"} src={HolydaySales} />
-                    </Box>
-                  </Flex>
-                </MenuList>
-              </Box>
-            </Portal>
-          </Menu>
-          <Button
-            fontWeight={"400"}
-            fontSize={"16px"}
-            backgroundColor={"transparent"}
-            _hover={{ backgroundColor: "trasparent" }}
-          >
-            Holyday Gift
-          </Button>
-          <Menu>
-            <MenuButton color={"rgb(51,51,51)"} lineHeight={"22.5px"}>
-              Open menu
-            </MenuButton>
-            <Portal>
-              <MenuList>
-                <MenuItem>Menu 1</MenuItem>
-                <MenuItem>New Window</MenuItem>
-                <MenuItem>Open Closed Tab</MenuItem>
-                <MenuItem>Open File</MenuItem>
-              </MenuList>
-            </Portal>
-          </Menu>
-        </Flex>
+        <NavbarTab />
+        </VStack>
       </VStack>
     </>
   );
